a242dd5efd7190622c8399da0a52bf10f9f82d58
Arush Kolluri <abcdk1519@gmail.com>
2023-02-03 22:14:14 +0530

    commit from develop

---
+++ src/MasterFile.java
@@ -0,0 +1,10 @@
+
+public class MasterFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("file newly created from develop branch and should be visible in master after merge ");
+	}
+
+}
