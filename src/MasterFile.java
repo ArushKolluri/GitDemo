67eb6d93776af6f4edafc79b34d40eb72c37236b
Arush Kolluri <abcdk1519@gmail.com>
2023-02-03 22:59:41 +0530

    new commit

--- src/MasterFile.java
+++ src/MasterFile.java
@@ -5,7 +5,7 @@ public class MasterFile {
 		// TODO Auto-generated method stub
 
 		System.out.println("file newly created from develop branch and should be visible in master after merge ");
-		System.out.println("git test from eclipse- master branch");
+		System.out.println("git test from eclipse- master branch recommit");
 	}
 
 }
